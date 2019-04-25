8bf38707061b72b866d0ff12da3727e41face0a8
ZwerOxotnik <zweroxotnik@gmail.com>
2019-04-25 18:36:37 +0300

    First commit

---
+++ prototypes/items.lua
@@ -0,0 +1,24 @@
+data:extend({
+ {
+    type = "item-with-entity-data",
+    name = "dirigible-blimp",
+    icon = "__M-Dirigible__/graphics/icon/blimp.png",
+	icon_size = 32,
+    flags = {},
+    subgroup = "transport",
+	order = "b[personal-transport]-e[gunship]",
+    place_result= "dirigible-blimp",
+    stack_size= 1,
+  },
+ {
+    type = "item-with-entity-data",
+    name = "dirigible-vacuum",
+    icon = "__M-Dirigible__/graphics/icon/vacuum.png",
+	icon_size = 32,
+    flags = {},
+    subgroup = "transport",
+	order = "b[personal-transport]-e[gunship]",
+    place_result= "dirigible-vacuum",
+    stack_size= 1,
+  }
+})
